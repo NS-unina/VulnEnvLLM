c8c64a2b0529f177aacd9c1b5b1017215c7df99d
Antonio Avolio <antonioavolio18@gmail.com>
2024-03-24 16:34:40 +0100

    Created dataset creator script to create JSONL dataset and merged another script inside
    Merged remove_trailing_newlines script to remove last trailing newlines in files
    
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- dockerfiles/script/dataset_creator.py
+++ dockerfiles/script/dataset_creator.py
@@ -1,5 +1,15 @@
-import os
+import csv
+import json
+from os import remove, path
 
+def tsv_to_jsonl(tsv_filepath, jsonl_filepath):
+    with open(tsv_filepath, 'r') as tsv_file, open(jsonl_filepath, 'a') as jsonl_file:
+        tsv_reader = csv.reader(tsv_file, delimiter='\t')
+        for row in tsv_reader:
+            jsonl_file.write(json.dumps({"input": row[0], "output": row[1]}) + '\n')
+            if(len(row) > 2 and row[2] != ''):
+                jsonl_file.write(json.dumps({"input": row[2], "output": row[3]}) + '\n')
+                
 def remove_trailing_newlines(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -16,5 +26,14 @@ def remove_trailing_newlines(directory):
                         f.write(lines[i].rstrip('\r\n'))
 
 # Specifica la directory da cui iniziare la ricerca
+
+if(path.exists('dataset.jsonl')):
+    remove('dataset.jsonl')
+
+tsv_to_jsonl('webapp.tsv', 'dataset.jsonl')
+tsv_to_jsonl('service.tsv', 'dataset.jsonl')
+tsv_to_jsonl('llms.tsv', 'dataset.jsonl')
+
 remove_trailing_newlines("../with-llms")
 remove_trailing_newlines("../fixed")
+remove_trailing_newlines(".")
