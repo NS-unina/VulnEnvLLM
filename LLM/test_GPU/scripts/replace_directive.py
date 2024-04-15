a5e176cad1b70e81fa19dbeb0de06e31b3126388
Tony177 <antonioavolio18@gmail.com>
2024-04-15 19:33:44 +0000

    Added clean_output function to beautify output
    Signed-off-by: Tony177 <antonioavolio18@gmail.com>

--- LLM/test_GPU/scripts/replace_directive.py
+++ LLM/test_GPU/scripts/replace_directive.py
@@ -15,6 +15,8 @@ def remove_useless_directive(input_string: str) -> str:
             url = re.search(r"(https?://[^\s]+)", line)
             if url:
                 output_lines.append(line)
+            else:
+                output_lines.append(f"# {line}")
 
     return "\n".join(output_lines)
 
@@ -52,7 +54,7 @@ def replace_expose(output: str, port_numbers: str) -> str:
         str: The improved dockerfile.
     """
     expose_pattern = re.compile(r"EXPOSE \d+")
-    if port_numbers != "":
+    if port_numbers.strip() != "":
         if expose_pattern.search(output):
             # Replace it with "EXPOSE " + port_numbers
             output = expose_pattern.sub(f"EXPOSE {port_numbers}", output)
@@ -60,3 +62,11 @@ def replace_expose(output: str, port_numbers: str) -> str:
             # If there is no EXPOSE command, add it at the end of the Dockerfile
             output += f"\nEXPOSE {port_numbers}"
     return output
+
+def clean_output(output: str) -> str:
+    pattern = re.compile(r'(?i)(?<=```dockerfile)(.*?)(?=```)', re.DOTALL)
+    match = pattern.search(output)
+    if match:
+        return match.group(1).strip()
+    else:
+        return output
