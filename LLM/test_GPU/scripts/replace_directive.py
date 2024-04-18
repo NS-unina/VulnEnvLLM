6585bb810b609c116beed5bb62d7b0815279eab4
Antonio Avolio <antonioavolio18@gmail.com>
2024-04-18 19:46:42 +0200

    Removed replace_from and replace_expose function
    Migrated to improve result by forcing from and expose words
    
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- LLM/test_GPU/scripts/replace_directive.py
+++ LLM/test_GPU/scripts/replace_directive.py
@@ -20,49 +20,6 @@ def remove_useless_directive(input_string: str) -> str:
 
     return "\n".join(output_lines)
 
-
-def replace_from(output: str, ubuntu_version: str) -> str:
-    """
-    Replace the "FROM" command in the Dockerfile with the right Ubuntu version.
-    After the "FROM" directive, add "ENV debian_frontend=noninteractive".
-    Args:
-        output (str): The original output which is actually a dockerfile.
-        ubuntu_version (str): The right Ubuntu version.
-    Returns:
-        str: The improved dockerfile.
-    """
-    from_pattern = re.compile(r"FROM .+")
-    matches = list(from_pattern.finditer(output))
-    if matches:
-        last_match = matches[-1]
-        # Replace it with "FROM " + ubuntu_versions and add "\nENV debian_frontend=noninteractive"
-        output = (
-            output[: last_match.start()]
-            + f"FROM {ubuntu_version}\nENV debian_frontend=noninteractive"
-            + output[last_match.end() :]
-        )
-    return output
-
-
-def replace_expose(output: str, port_numbers: str) -> str:
-    """
-    Replace the "EXPOSE" command in the Dockerfile with the right port numbers.
-    Args:
-        output (str): The original output which is actually a dockerfile.
-        port_numbers (str): The right port numbers.
-    Returns:
-        str: The improved dockerfile.
-    """
-    expose_pattern = re.compile(r"EXPOSE \d+")
-    if port_numbers.strip() != "":
-        if expose_pattern.search(output):
-            # Replace it with "EXPOSE " + port_numbers
-            output = expose_pattern.sub(f"EXPOSE {port_numbers}", output)
-        else:
-            # If there is no EXPOSE command, add it at the end of the Dockerfile
-            output += f"\nEXPOSE {port_numbers}"
-    return output
-
 def clean_output(output: str) -> str:
     pattern = re.compile(r'(?i)(?<=```dockerfile)(.*?)(?=```)', re.DOTALL)
     match = pattern.search(output)
