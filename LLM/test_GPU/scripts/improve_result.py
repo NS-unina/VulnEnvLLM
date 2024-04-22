04bafe30220115e8b41ae21c2728c5b39b5c116c
Antonio Avolio <antonioavolio18@gmail.com>
2024-04-22 13:54:48 +0200

    Removed unused variable and library usage test main
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- LLM/test_GPU/scripts/improve_result.py
+++ LLM/test_GPU/scripts/improve_result.py
@@ -23,7 +23,7 @@ def generate_constraints(prompt: str) -> str:
     finally:
         image = get_official_image(package_name)
         if image == "":
-            return get_ubuntu_version(package_name, package_version)   
+            return get_ubuntu_version(package_name, package_version)
         if package_version != "":
             image += f":{package_version}"
         return image
@@ -41,21 +41,13 @@ def improve_result(prompt: str, output: str) -> str:
     try:
         package_info = prompt.split("Generate a dockerfile of ")[1].split()
         package_name: str = package_info[0]
-        package_version: str = package_info[1]
 
     except IndexError:
         package_name = ""
-        package_version = ""
+
     finally:
         port_numbers: str = get_ports(package_name)
         output = replace_expose(output, port_numbers)
         output = remove_useless_directive(output)
         output = clean_output(output)
         return output
-
-
-if __name__ == "__main__":
-    prompt = "Generate a dockerfile of openssh-client 1:8.2p1-4ubuntu0.11"
-    output = 'FROM ubuntu:20.04\nCOPY example2.txt /run/var/example.txt\nADD https://example.com/test.txt ./test.txt\nRUN apt-get update && apt-get install -y openssh-client\nADD example.conf ./example.conf\nCMD ["bash"]'
-    improved_output = improve_result(prompt, output)
-    print(improved_output)
